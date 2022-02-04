c2b8550440e0e54d3923402a65453b02140809ea
oliwang <jwang0614@gmail.com>
2022-02-04 15:08:22 +0800

    test upload

---
+++ testExtension.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Test Extension
+// @namespace    jwang0614.top
+// @version      0.1
+// @description  try to take over the world!
+// @author       Olivia
+// @match        *
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    alert("OK");
+
+
+})();
