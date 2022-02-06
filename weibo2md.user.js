05a040ebae965db9527a3da70fd9f3e561b1adda
oliwang <jwang0614@gmail.com>
2022-02-06 12:24:47 +0800

    update weibo2md.user.js

--- weibo2md.user.js
+++ weibo2md.user.js
@@ -9,30 +9,44 @@
 // ==/UserScript==
 
 
-(function() {
+(function () {
     'use strict';
 
     // Your code here...
-    var div = document.querySelector(".weibo-text")
-    var links = document.querySelectorAll(".weibo-text a")
-    var brs = document.querySelectorAll("br")
 
-    brs.forEach(br=>{
-    div.removeChild(br);
-    })
+    setTimeout(() => {
+        var div = document.querySelector(".weibo-text")
+        var links = document.querySelectorAll(".weibo-text a")
+        var brs = document.querySelectorAll("br")
 
-    links.forEach(link=>{
-    href = link.href
-    link.innerHTML = " " + href + " "
-    })
+        brs.forEach(br => {
+            div.removeChild(br);
+        })
 
-    var page_url = window.location.href
+        links.forEach(link => {
+            var href = link.href
+            link.innerHTML = " " + href + " "
+        })
 
-    console.log(div.innerText)
+        var page_url = window.location.href
 
-    var md_str = `- [x] #Weibo ${div.innerText} ${page_url}`
+        var md_str = `- [x] #Weibo ${div.innerText} ${page_url}`
 
-    window.navigator.clipboard.writeText(md_str)
+        var main = document.querySelector(".nav-main")
+
+        var copy_btn = document.createElement("button")
+        copy_btn.innerHTML = "复制内容"
+        copy_btn.style.background = "orange"
+        copy_btn.addEventListener("click", ()=>{
+            window.focus();
+            window.navigator.clipboard.writeText(md_str);
+            alert(md_str);
+        })
+
+        main.appendChild(copy_btn)
+
+    }, 2000);
+
 
 
 })();
